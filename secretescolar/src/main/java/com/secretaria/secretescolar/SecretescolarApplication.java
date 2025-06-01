b67f0e1357d16f2d77f7db8038da8092c63c34c6
Maila <cardmaila19@gmail.com>
2025-05-31 23:30:38 -0300

    Reseting

---
+++ secretescolar/src/main/java/com/secretaria/secretescolar/SecretescolarApplication.java
@@ -0,0 +1,13 @@
+package com.secretaria.secretescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretescolarApplication.class, args);
+	}
+
+}
